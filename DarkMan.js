c71d69c1a5e8e604629bc7b2e5f1c2adf37f3c14
esobadawa0-star <esobadawa0@gmail.com>
2026-05-02 12:59:39 +0300

    Update DarkMan.js

--- DarkMan.js
+++ DarkMan.js
@@ -7387,7 +7387,7 @@ message.member.voiceChannel
 
 //تۆكینه‌كان لێره‌ جا بنێ
 
-client.login("");
+client.login("ODE4MDKxMDlwNjkxOTYzOTM0.YEUJCg.apauPkXJDodfDEoAB8mf0wqX_N8");
 client2.login("");
 client3.login("");
 client4.login("");
